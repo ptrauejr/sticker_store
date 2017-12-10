492755e7fb28f75bb00d1ffc14ef91b9ed6e5560
Paul Traue <ptrauejr@gmail.com>
2017-12-10 15:40:09 -0600

    Adds default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
-  get 'store/index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to 'store#index'
 end
