bb95e2ac0c94530479cc6c494667de5ad0209fba
Paul Traue <ptrauejr@gmail.com>
2017-12-16 18:35:51 -0600

    Sets the state

--- app/javascript/packs/Store/index.js
+++ app/javascript/packs/Store/index.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import ProductsList from './ProductsList'
+import Cart from './Cart'
 
 class Store extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            products: this.props.products,
+            cart: {
+                items: [],
+                total: 0
+            }
+        }
+    }
+
     render() {
         return (
             <div>
-                <Cart />
-                <ProductsList products={this.props.products} />
+                <Cart cart={this.state.cart} />
+                <ProductsList products={this.state.products} />
             </div>
         )
     }
