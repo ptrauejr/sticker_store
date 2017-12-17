1d2013699fb4b201f2f22d43c030acd91edbe089
Paul Traue <ptrauejr@gmail.com>
2017-12-16 19:17:21 -0600

    Adds price information

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Sticker from './Sticker'
 
-const Product = ({product}) =>
-    <div className="product">
-        <Sticker product={product} />
-        <div clasName="cartControls">
-            <button>+ Add to cart</button>
-            <button>- Remove from cart</button>
-        </div>
-
-    </div>
+class Product extends Component {
+    render() {
+        return (
+            <div className="product">
+                <Sticker product={this.props.product} />
+                <div className="cartControls">
+                    <p className="price">${(this.props.product.skus.data[0].price/100).toFixed(2)}</p>
+                    <button>+ Add to cart</button>
+                    <button>- Remove from cart</button>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default Product
