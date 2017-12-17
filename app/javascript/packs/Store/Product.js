310ff125dd95b2bdfd70f6ea5307f554ef9da39d
Paul Traue <ptrauejr@gmail.com>
2017-12-16 19:03:52 -0600

    Starts moving the sticker stuff to a seperate component

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -2,13 +2,7 @@ import React from 'react'
 
 const Product = ({product}) =>
     <div className="product">
-        <h2>{product.name}</h2>
-        <h3>{product.caption}</h3>
-        <div className="sticker">
-            <img src={product.images[0]} alt={product.name} />
-        </div>
-        <h3>Free shipping anywhere in the world!</h3>
-
+        <Sticker product={product} />
         <div clasName="cartControls">
             <button>+ Add to cart</button>
             <button>- Remove from cart</button>
