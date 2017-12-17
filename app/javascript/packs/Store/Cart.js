70847b27e9f7904bba2b928647feee8422129ac0
Paul Traue <ptrauejr@gmail.com>
2017-12-17 15:17:18 -0600

    Adds the StripeCheckout component to the cart

--- app/javascript/packs/Store/Cart.js
+++ app/javascript/packs/Store/Cart.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import StripeCheckout from 'react-stripe-checkout'
 
-const Cart = ({cart}) => (
+const Cart = ({cart, onToken}) => (
     <div className="cart">
         Total: ${cart.total/100}
+        {cart.total > 0 &&
+            <StripeCheckout
+                token={onToken}
+                stripeKey="pk_test_8ndRxC8GGO2deOHuyOEsVDGn"
+                description="Stickers from stickers store"
+                amount={cart.total}
+                label="Checkout"
+                billingAddres={true}
+                shippingAddress={true} />
+        }
     </div>
 )
 
