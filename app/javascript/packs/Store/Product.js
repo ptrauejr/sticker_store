4c24155db341343d04e69d862bbfc3d13ebf14f6
Paul Traue <ptrauejr@gmail.com>
2017-12-16 18:17:17 -0600

    Adds more product information to the Products component

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const Product = ({product}) =>
-    <div>
+    <div class="product">
+        <h2>{product.name}</h2>
+        <h3>{product.caption}</h3>
+        <div class="sticker">
+            <img src={product.images[0]} alt={product.name} />
+        </div>
+        <h3>Free shipping anywhere in the world!</h3>
     </div>
 
 export default Product
