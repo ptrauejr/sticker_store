71cacf0733773cbcc4d50ff39076b2245f5939e5
Paul Traue <ptrauejr@gmail.com>
2017-12-16 18:24:04 -0600

    Changes some class tags to className tags to be react compliant

--- app/javascript/packs/Store/Product.js
+++ app/javascript/packs/Store/Product.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 const Product = ({product}) =>
-    <div class="product">
+    <div className="product">
         <h2>{product.name}</h2>
         <h3>{product.caption}</h3>
-        <div class="sticker">
+        <div className="sticker">
             <img src={product.images[0]} alt={product.name} />
         </div>
         <h3>Free shipping anywhere in the world!</h3>
